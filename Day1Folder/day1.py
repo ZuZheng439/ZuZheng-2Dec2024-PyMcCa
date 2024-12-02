381100d3403409835cdc9b6f0e85b342c77dba5f
ZuZheng439 <chong_zu_zheng@students.edu.sg>
2024-12-02 04:29:18 +0000

    Day 1
    Co-authored-by: Computhink-Alfred <Computhink-Alfred@users.noreply.github.com>

--- Day1Folder/day1.py
+++ Day1Folder/day1.py
@@ -1,4 +1,35 @@
 # Write all your codes for Day 1 here.
 # REMEMBER to change main.py to import this file.
 # You can COMMENT out the previous task before going on to the next task
-print("hello from day1")
+# print("hello from day1")
+# print("my name is Zu Zheng")
+# print("i am 13 years old")
+# print("i attend ACS BARKER")    
+# print("i would buy new games") 
+
+# animal1 = "tiger"
+# print(animal1)
+# animal2 = "zebra"
+# print(animal2)
+# animal3 = "elephant"
+# print(animal3)
+# tiger = 24
+# print(tiger)
+# zebra =23
+# print(zebra)
+# elephant = 8
+# print(elephant)
+# print(ans)
+# def selfintro(name, age):
+#     print("my name is "+name)
+#     print("i am "+ age + " years old")
+#     print("i attend ACS Barker")
+#     print("i would buy new games")
+
+# selfintro("john", "13")
+# selfintro("jack", "14")
+# selfintro("zack", "15")
+def sumOf(num1, num2):
+    print(num1 + num2)
+
+sumOf(24 ,15)
